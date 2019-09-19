567840ecce4e3f2dbccfd568ba8128c5d3625b65
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-19 02:01:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,8 +38,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-wow = array.shift
-
+p wow = array.shift
 end
 
 def retrieve_last_element_from_array(array)
